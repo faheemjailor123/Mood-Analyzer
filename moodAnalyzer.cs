d6bbac547b2cd38368437e9e31a5f98b39db8389
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-11 09:08:16 +0530

    welcome

---
+++ moodAnalyzer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MoodAnalyser01_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the mood analyser program");
+        }
+    }
+}
